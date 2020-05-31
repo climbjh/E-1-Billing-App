c3ae1150cde7bdc756c39c061879555013864e79
Whit Matthews <whitmell@gmail.com>
2020-05-31 01:44:45 -0400

    Changed name to em-billing

--- em-billing/e2e/src/app.e2e-spec.ts
+++ em-billing/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-basic app is running!');
+    expect(page.getTitleText()).toEqual('Evan\'s billing app is running!');
   });
 
   afterEach(async () => {
